56071fe38ae4b9053001e4854a290110190a43a7
Marco Hernandez <marcoah@outlook.com>
2025-10-14 20:43:11 -0300

    Update CSV file path and add example data for processing in prog4.py Add prog8.py for chloropleth map visualization using Altair

--- prog4.py
+++ prog4.py
@@ -5,7 +5,7 @@ Carga de archivo CSV
 """
 import csv
  
-with open('example.csv', newline='') as File:  
+with open('data/example.csv', newline='') as File:  
     reader = csv.reader(File)
     for row in reader:
         print(row)
---
+++ prog8.py
@@ -0,0 +1,56 @@
+"""
+=========================
+Mapa cloropetico
+=========================
+
+DESC
+
+libreria https://altair-viz.github.io/
+
+"""
+
+import altair as alt
+from vega_datasets import data
+
+df_wind = data.windvectors()
+data_world = alt.topo_feature(data.world_110m.url, "countries")
+
+wedge = alt.Chart(df_wind).mark_point(shape="wedge", filled=True).encode(
+    alt.Latitude("latitude"),
+    alt.Longitude("longitude"),
+    alt.Color("dir")
+        .scale(domain=[0, 360], scheme="rainbow")
+        .legend(None),
+    alt.Angle("dir").scale(domain=[0, 360], range=[180, 540]),
+    alt.Size("speed").scale(rangeMax=500)
+).project("equalEarth")
+
+xmin, xmax, ymin, ymax = (
+    df_wind.longitude.min(),
+    df_wind.longitude.max(),
+    df_wind.latitude.min(),
+    df_wind.latitude.max(),
+)
+
+# extent as feature or featurecollection
+extent = {
+    "type": "Feature",
+    "geometry": {"type": "Polygon",
+                 "coordinates": [[
+                     [xmax, ymax],
+                     [xmax, ymin],
+                     [xmin, ymin],
+                     [xmin, ymax],
+                     [xmax, ymax]]]
+                },
+    "properties": {}
+}
+
+# use fit combined with clip=True
+base = (
+    alt.Chart(data_world)
+    .mark_geoshape(clip=True, fill="lightgray", stroke="black", strokeWidth=0.5)
+    .project(type="equalEarth", fit=extent)
+)
+
+base + wedge
